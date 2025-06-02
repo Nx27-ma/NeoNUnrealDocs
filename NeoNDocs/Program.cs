808ec49c27eac56418c647f2d0bb2352a24e7d73
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-06-02 15:39:02 +0200

    added json through DI + json namespace added + sg base + json doesnt properly load yet

--- NeoNDocs/Program.cs
+++ NeoNDocs/Program.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NeoNDocs.Json;
+using System.Diagnostics;
 
 namespace NeoNDocs;
 
 public class Program
 {
-    public static async Task Main(string[] args)
-    {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+  public static async Task Main(string[] args)
+  {
+    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    builder.Services.AddScoped<LoadRequestedJson>();
 
-        await builder.Build().RunAsync();
-    }
+    await builder.Build().RunAsync();
+  }
 }
